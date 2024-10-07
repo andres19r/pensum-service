a00b9aabfe21899aafb7e1f89b615b5de8336670
Andres Rivero <andres19rivero@gmail.com>
2024-10-07 00:15:21 -0400

    fix(models): add unique constraint to pensum field and remove from semester

--- src/data/mongo/models/pensum.model.ts
+++ src/data/mongo/models/pensum.model.ts
@@ -3,10 +3,12 @@ import mongoose, { Schema } from "mongoose";
 const pensumSchema = new mongoose.Schema({
   career: {
     type: String,
+    unique: true,
     required: [true, "Career is required"]
   },
   university: {
     type: String,
+    unique: true,
     required: [true, "University is required"]
   },
   semesters: [{
