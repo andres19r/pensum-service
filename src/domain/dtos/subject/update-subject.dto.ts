8d34cf3f8be05394d8da2069a4ad1268f545e815
Andres Rivero <andres19rivero@gmail.com>
2024-11-24 23:58:05 -0400

    feat(subjects): implement update-subject dto

---
+++ src/domain/dtos/subject/update-subject.dto.ts
@@ -0,0 +1,28 @@
+import {CreateSubjectDto} from "./create-subject.dto";
+
+export class UpdateSubjectDto {
+    private constructor(
+        public readonly name?: string,
+        public readonly semester?: string,
+        public readonly pensumId?: string,
+        public readonly score?: number,
+        public readonly state?: string,
+    ) {
+    }
+
+    get values(): { [key: string]: any[] } {
+        const returnObj: { [key: string]: any } = {};
+
+        if (this.name) returnObj.name = this.name;
+        if (this.semester) returnObj.semester = this.semester;
+        if (this.pensumId) returnObj.pensumId = this.pensumId;
+        if (this.score) returnObj.score = this.score;
+        if (this.state) returnObj.state = this.state;
+        return returnObj;
+    }
+
+    static create(props: { [key: string]: any }): UpdateSubjectDto {
+        const {name, semester, pensumId, score, state} = props;
+        return new UpdateSubjectDto(name, semester, pensumId, score, state);
+    }
+}
